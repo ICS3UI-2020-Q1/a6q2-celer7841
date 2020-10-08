4786c3eb02eda7713c3f45b7a77eadb5d17383c7
celer7841 <celer7841@wrdsb.ca>
2020-10-08 22:26:33 +0000

    finished

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
  *
- * @author 
+ * @author celer7841
  */
 public class Main {
 
@@ -9,7 +10,23 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // create the Scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // create the array to store all the grades
+    System.out.println("Please enter in 10 integers to put into the array");
+    final int NUMINTEGERS = 10;
+    int[] numbers = new int [NUMINTEGERS];
+    for(int i = 0; i < NUMINTEGERS; i++){
+      numbers[i] = input.nextInt();
+    }System.out.println("Please enter a number to find");
+    for(int i = 0; i < NUMINTEGERS; i++){
+    int newnumber = input.nextInt();
+    for(i = 0; i < NUMINTEGERS; i++){
+      if(newnumber == numbers [i]){
+        System.out.println(newnumber + " is located at index " + i );
+      }
+      }
+    }
   }
-}
+      }
